37de15938bcd7f74e6aa0d04dd187cbd91f48e36
Nikita DM <domitrak.work@gmail.com>
2022-10-16 12:07:13 +0300

    finish_project

---
+++ src/js/modules/changeModalState.js
@@ -0,0 +1,50 @@
+import checkNumInputs from './checkNumInputs';
+
+const changeModalState = (state) => {
+    const windowForm = document.querySelectorAll('.balcon_icons_img'),
+        windowWidth = document.querySelectorAll('#width'),
+        windowHeight = document.querySelectorAll('#height'),
+        windowType = document.querySelectorAll('#view_type'),
+        windowProfile = document.querySelectorAll('.checkbox');
+
+    checkNumInputs('#width');
+    checkNumInputs('#height');
+
+    function bindActionToParams(event, elem, prop) {
+        elem.forEach((item, i) => {
+            item.addEventListener(event, () => {
+                switch (item.nodeName) {
+                    case 'SPAN':
+                        state[prop] = i;
+                        break;
+                    case 'INPUT':
+                        if (item.getAttribute('type') === 'checkbox') {
+                            i === 0 ? state[prop] = 'cold' : state[prop] = 'warm';
+                            elem.forEach((box, j) => {
+                                box.checked = false;
+                                if (i == j) {
+                                    box.checked = true;
+                                }
+                            });
+                        } else {
+                            state[prop] = item.value;
+                        }
+                        break;
+                    case 'SELECT':
+                        state[prop] = item.value;
+                        break;
+                }
+
+                console.log(state);
+            });
+        });
+    }
+
+    bindActionToParams('click', windowForm, 'form');
+    bindActionToParams('input', windowHeight, 'height');
+    bindActionToParams('input', windowWidth, 'width');
+    bindActionToParams('change', windowType, 'type');
+    bindActionToParams('change', windowProfile, 'profile');
+};
+
+export default changeModalState;
